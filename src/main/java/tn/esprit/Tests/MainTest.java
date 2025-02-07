ffa0bbc0c356614fd58ec5888a428afb13ac8ea6
saracher <cherif.sarra@esprit.tn>
2025-02-07 12:27:52 +0100

    initial commit

---
+++ src/main/java/tn/esprit/Tests/MainTest.java
@@ -0,0 +1,7 @@
+package tn.esprit.Tests;
+
+public class MainTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/tn/esprit/Utils/MyDataBase.java
@@ -0,0 +1,4 @@
+package tn.esprit.Utils;
+
+public class MyDataBase {
+}
